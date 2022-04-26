3284d719276a673aa4904fc44b2721ab9f8f62c0
Sebastian Ornelas <sebastianornelas@berkeley.edu>
2022-04-26 13:55:03 -0700

    added /me route in /user, returning user JSON

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -154,4 +154,14 @@ router.post(
     }
   );
 
+router.get("/me", auth, async (req, res) => {
+  try {
+    // request.user is getting fetched from Middleware after token authentication
+    const user = await User.findById(req.user.id);
+    res.json(user);
+  } catch (e) {
+    res.send({ message: "Error in Fetching user" });
+  }
+});
+
 module.exports = router;
